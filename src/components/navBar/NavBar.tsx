a886251b6457e6f6279b4267f78e7334d47b31c0
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-12 20:43:08 +0900

    [style] 네비바 아이콘 설정

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,11 +1,31 @@
 import { useNavigationStore } from "@/store/navigationStore";
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import styles from "./NavBar.module.scss";
 
+//아이콘
+import { IconType } from "react-icons";
+import {
+  IoHomeOutline,
+  IoSearchOutline,
+  IoAddOutline,
+  IoPaperPlaneOutline,
+  IoPersonOutline,
+} from "react-icons/io5";
+
+// 아이콘 매핑 객체
+const iconMap: Record<string, IconType> = {
+  home: IoHomeOutline,
+  search: IoSearchOutline,
+  write: IoAddOutline, // 글쓰기 아이콘
+  message: IoPaperPlaneOutline,
+  profile: IoPersonOutline,
+};
+
 function NavBar() {
   const { navigation, fetchNavigation } = useNavigationStore();
+  const loaction = useLocation();
 
   useEffect(() => {
     fetchNavigation();
@@ -13,11 +33,19 @@ function NavBar() {
 
   return (
     <nav className={styles.navbar}>
-      {navigation.map((navItem) => (
-        <Link key={navItem.id} to={navItem.path}>
-          {navItem.label}
-        </Link>
-      ))}
+      {navigation.map((navItem) => {
+        const IconComponent = iconMap[navItem.label]; // 아이콘 동적 매핑
+        const isActive = loaction.pathname === navItem.path;
+        return (
+          <Link
+            key={navItem.id}
+            to={navItem.path}
+            className={`${styles.navItem} ${isActive ? styles.active : ""}`}
+          >
+            {IconComponent && <IconComponent />}
+          </Link>
+        );
+      })}
     </nav>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import styles from "./home.module.scss";
 
 function index() {
   return (
-    <div className={styles.page}>
+    <div>
       <h1>홈페이지 </h1>
     </div>
   );
