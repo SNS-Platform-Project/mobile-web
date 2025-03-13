426b300ab48d45a8fb181866b1f14800f8cd5d7f
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-13 10:36:55 +0900

    [feate] 프로필 내용 하드코딩.

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -4,8 +4,39 @@ import styles from "./profile.module.scss";
 
 function index() {
   return (
-    <div>
+    <div className={styles.profileContainer}>
       <Header type="profile" />
+
+      {/* 프로필 상단 영역 */}
+      <div className={styles.profileHeader}>
+        {/* 프로필 이미지 */}
+        <img
+          src="src/assets/images/HJY.jpg"
+          alt="프로필 이미지"
+          className={styles.profileImage}
+        />
+
+        {/* 유저 정보 및 팔로잉/팔로워 수 */}
+        <div className={styles.profileInfo}>
+          <p className={styles.username}>한주영</p>
+          <div className={styles.followContainer}>
+            <span>
+              <b>350</b> 팔로워
+            </span>
+            <span>
+              <b>150</b> 팔로잉
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* 소개글 */}
+      <p className={styles.bio}>안녕하세요</p>
+
+      <div>
+        <button className={styles.editButton}>프로필 편집</button>
+        <button className={styles.editButton}>프로필 공유</button>
+      </div>
     </div>
   );
 }
