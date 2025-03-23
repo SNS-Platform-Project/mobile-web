0a9144bfef472865d34a5111e5c016ba82414e38
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-23 21:31:48 +0900

    [feat] 컨텐츠 컴포넌트 생성 (유저, 시간, 멘션, 태그)

---
+++ src/pages/post/detail/components/content/PostContent.tsx
@@ -0,0 +1,65 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import "dayjs/locale/ko";
+
+import styles from "./PostContent.module.scss";
+
+dayjs.extend(relativeTime);
+dayjs.locale("ko");
+
+const mockPost = {
+  id: 1,
+  type: "post",
+  user: {
+    userId: "abc124",
+    username: "한주영",
+    profileImageUrl:
+      "https://ui-avatars.com/api/?name=한주영&background=random&rounded=true​",
+  },
+  createdAt: "2025-03-22T02:56:04.470+00:00",
+  content: "안녕하세요 @han #프론트엔드 #리액트 개발자입니다!",
+  hashtags: ["프론트엔드", "리액트"],
+  mentions: ["han"],
+  images: ["~"],
+};
+
+function PostContent() {
+  const { user, createdAt, content } = mockPost;
+  const formattedTime = dayjs(createdAt).fromNow(); // "2일 전" 등
+
+  const parsedContent = content.split(/(\s+)/).map((part, index) => {
+    if (/^@[\wㄱ-ㅎㅏ-ㅣ가-힣]+$/.test(part)) {
+      return (
+        <span key={index} className={styles.mention}>
+          {part}
+        </span>
+      );
+    } else if (/^#[\wㄱ-ㅎㅏ-ㅣ가-힣]+$/.test(part)) {
+      return (
+        <span key={index} className={styles.hashtag}>
+          {part}
+        </span>
+      );
+    }
+    return <span key={index}>{part}</span>;
+  });
+
+  return (
+    <div className={styles.postContent}>
+      <div className={styles.meta}>
+        <img
+          className={styles.profileImage}
+          src={user.profileImageUrl}
+          alt={`${user.username}의 프로필`}
+        />
+        <div className={styles.userInfo}>
+          <span className={styles.username}>{user.username}</span>
+          <span className={styles.time}> {formattedTime}</span>
+        </div>
+      </div>
+      <div className={styles.text}>{parsedContent}</div>
+    </div>
+  );
+}
+
+export default PostContent;
--- src/pages/post/detail/index.tsx
+++ src/pages/post/detail/index.tsx
@@ -1,11 +1,14 @@
 import PostHeader from "./components/header/PostHeader";
 
+import PostContent from "./components/content/PostContent";
+
 import styles from "./index.mdoule.scss";
 
 function Index() {
   return (
     <div>
       <PostHeader />
+      <PostContent />
     </div>
   );
 }
