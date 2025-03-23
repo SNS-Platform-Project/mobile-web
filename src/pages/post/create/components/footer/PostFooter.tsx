d2be88b34c06d1495adbee855d12f17e83219d74
ZOODUDDLDI <wndud238561@gmail.com>
2025-03-23 15:28:20 +0900

    [feat] 나중에 해야할 기능 주석

--- src/pages/post/create/components/footer/PostFooter.tsx
+++ src/pages/post/create/components/footer/PostFooter.tsx
@@ -1,6 +1,7 @@
 import styles from "./PostFooter.module.scss";
 
 function PostFooter() {
+  // TODO: 모바일 키보드 해야한다.
   return (
     <div className={styles.postFooter}>
       <button className={styles.postFooter__submit}>게시</button>
